e14ce9089f9199e9ab68f295ab6766be9071f523
dmuldrew <dmuldrew@gmail.com>
2017-07-31 17:51:18 -0700

    added more server boilerplate

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 'use strict';
 
+const pg = require('pg');
 const express = require('express');
-const app = express();
-const proxy = require('express-request-proxy');
-const PORT = process.env.PORT || 3000;
-const githubToken = process.env.API_KEY;
+const bodyParser = require('body-parser');
+const request = require('request');
 
+const PORT = process.env.PORT || 3000;
+const apiKey = process.env.API_KEY;
+
+const conString = ''
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
-
-
-app.get('/themoviedb/*', function (request, response) {
-  (
-    proxy({
-      url: `https://api.themoviedb.org/${request.params[0]}`,
-      headers: {
-        Authorization: `key ${apiKey}`
-      }
-    })
-  )(request, response);
-})
-
-app.get('/index', function (req, res) {
-  res.sendFile('index.html', {root: './public'});
-})
 
 app.listen(PORT, function() {
   console.log(`Server started on ${PORT}`);
