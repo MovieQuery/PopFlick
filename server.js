1d5b532efffb29beac7ff992340e12a3ecfe8da4
ql24 <qlalberta@gmail.com>
2017-08-01 10:17:49 -0700

    set up local movie and user tables

--- server.js
+++ server.js
@@ -23,3 +23,29 @@ app.use(express.static('./public'));
 app.listen(PORT, function() {
   console.log(`Server started on ${PORT}`);
 });
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    movies (
+      movie_id SERIAL PRIMARY KEY,
+      title VARCHAR(255) UNIQUE NOT NULL,
+      release_date DATE,
+      synopsis TEXT NOT NULL,
+      poster_url VARCHAR(255)
+    );`
+  )
+  .then(loadMovies)
+  .catch(console.error);
+
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    users (
+      user_id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      movie_id INTEGER NOT NULL REFERENCES movies(movie_id)
+    );`
+  )
+  .then(loadMovies)
+  .catch(console.error);
+}
