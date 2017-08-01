d77b4bbe19dc0e3dace0f4f39bd18f74fcc7068e
dmuldrew <dmuldrew@gmail.com>
2017-07-31 17:55:02 -0700

    added database environment variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const request = require('request');
 const PORT = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY;
 
-const conString = ''
+var conString = process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
