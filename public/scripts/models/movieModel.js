4ce0369875a9ae7e15f54e8e046833c48761d835
David Broxton <broxtondude@gmail.com>
2017-08-01 14:51:31 -0700

    Commented out unused script/link tags in index.html. Added TODOs into index.html. Fixed file paths in index.html.

--- public/scripts/models/movieModel.js
+++ public/scripts/models/movieModel.js
@@ -1,14 +1,20 @@
 'use strict';
 
+var app = app || {};
+
 (function(module){
-  movieModel = {
+  var movieModel = {
     movieInputs: [],
     movieData: []
   };
 
 
 
-
+  $('#submitInput').on('click', function(event) {
+    event.preventDefault();
+    movieModel.movieInputs = $('.movieInput').val();
+    console.log(movieModel.movieInputs)
+  });
 
   module.movieModel = movieModel;
 })(app)
