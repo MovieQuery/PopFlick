b303c9a0e85c9dc2638ca9c9f73ef113c6546cf0
Tyler W <tyler.walser@yahoo.com>
2017-08-01 17:50:53 -0700

    we added handlebars to index.html and to resultsView.js

--- public/scripts/views/resultsView.js
+++ public/scripts/views/resultsView.js
@@ -4,5 +4,10 @@ var app = app || {};
 
 (function(module) {
 
+  var resultsData = app.movieData[0]
+  module.displayMovie = function (resultsData) {
+    const template = Handlebars.compile($('#resultsTemplate').html());
+    $('#results').append(template(data));
 
+  }
 })(app);
