b92317703547725791747306f3402924035f73ec
Tyler W <tyler.walser@yahoo.com>
2017-08-02 11:54:50 -0700

    made button dynamically in jquery instead of html

--- public/scripts/models/movieModel.js
+++ public/scripts/models/movieModel.js
@@ -17,7 +17,13 @@ var app = app || {};
 
     module.movieQuery(app.displayMovie); //Insert callback as parameter
     app.resultsController.initResultsView();
+    
   });
+
+  // $('.watchedButton').on('click', function(event){
+  //
+  //   //call movie query again for second option
+  // })
 
 // ajax call to send inputed titles to server
   module.movieQuery = function (callback){
@@ -26,15 +32,25 @@ var app = app || {};
       url: `/search?titles=${module.movieInputs}`,
       method: 'GET',
     })
-    // .then(console.log(data)
     .then(function (data) {
-      // var resultsData = data[0];
-      console.log('in movieQuery');
       console.log(data);
       module.movieData = data[0];
-      console.log(data[0]);
       callback(module.movieData);
     });
   }
+
+  module.watchedMovie = function (callback) {
+    $.ajax({
+      url:`/movies=${module.movieData[0]}`,
+      method: 'POST',
+    })
+    .then(function(data){
+      console.log('in POST:');
+      console.log(data);
+      module.movieData = data[1];
+      callback(module.movieData);
+    })
+  }
+
 
 })(app);
