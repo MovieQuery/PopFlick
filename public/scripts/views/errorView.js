a5ade9ea037932776ad314c5fd90452fe16441f9
David Broxton <broxtondude@gmail.com>
2017-08-04 09:52:06 -0700

    Fixes broken image at results screen

--- public/scripts/views/errorView.js
+++ public/scripts/views/errorView.js
@@ -7,7 +7,7 @@ var app = app || {};
 
   errorView.prependErrorMessage = function() {
     $('.mainArea').prepend(
-      '<div id="errorSection"><h1>Sorry, that movie doesn\'t seem to exist. Please try again.</h1> <img src="../../image/spilledpopcorn.jpg"><input id="restart" type="submit" value="Try Again"></div>');
+      '<div id="errorSection"><h1>Sorry, that movie doesn\'t seem to exist OR you have exhausted all of your recommendations for this title. Please try another movie.</h1> <img src="../../image/spilledpopcorn.jpg"><input id="restart" type="submit" value="Try Again"></div>');
   }
 
   module.errorView = errorView;
